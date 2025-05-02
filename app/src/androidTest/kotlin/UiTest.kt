c25a562335b3d6a25b41bb6354bfa374fec00d7f
JacobLevin0 <160152211+JacobLevin0@users.noreply.github.com>
2025-05-02 11:21:54 -0400

    git issues fixed?

--- app/src/androidTest/kotlin/UiTest.kt
+++ app/src/androidTest/kotlin/UiTest.kt
@@ -1,4 +1,11 @@
+import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.example.bibliotrack.MyApp
+import com.example.bibliotrack.navigation.BookNavigation
+import com.example.bibliotrack.ui.theme.AppTheme
 import org.junit.Rule
 import org.junit.Test
 
@@ -7,5 +14,22 @@ class UITest {
     @get:Rule
     val  composeTestRule = createComposeRule()
 
+    @Test
+    fun typeInSearch(){
+
+        composeTestRule.setContent {
+            AppTheme {
+                MyApp {
+                    BookNavigation()
+                }
+            }
+        }
+
+        composeTestRule.onNodeWithText("Start")
+            .performClick()
+
+        composeTestRule.onNode(hasSetTextAction())
+            .performTextInput("T")
+    }
 
 }
